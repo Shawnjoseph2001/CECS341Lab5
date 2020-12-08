14f2e444cf536460646209e35bf6d1f65e1b61f3
Shawnjoseph2001 <shawnjoseph2001@gmail.com>
2020-12-07 20:56:40 -0800

    /

--- project_5/project_5.srcs/sources_1/imports/Datapath.srcs/sources_1/new/Control.v
+++ project_5/project_5.srcs/sources_1/imports/Datapath.srcs/sources_1/new/Control.v
@@ -31,13 +31,13 @@ module Control(
     always @(*) begin
     case(Op)
     6'b0:begin
-    RegWrite <= 1'b1;
-    RegDst <= 1'b1;
-    Branch <= 2'b0;
-    MemRead <= 1'b0;
-    MemToReg <= 1'b0;
-    MemWrite <= 1'b0;
-    ALUSrc <= 1'b0;
+    RegWrite <= 1;
+    RegDst <= 1;
+    Branch <= 0;
+    MemRead <= 0;
+    MemToReg <= 0;
+    MemWrite <= 0;
+    ALUSrc <= 0;
     case(Func)
     6'h20:begin //add
     ALUCntl <= 4'b1010;
@@ -70,121 +70,110 @@ module Control(
     ALUCntl <= 4'b0100;
     end
     default:begin //invalid code
-    //$display("Error! Check control file");
+    $display("Error! Check control file");
     end
     endcase
     end
     6'h08:begin //addi
-    RegWrite <= 1'b1;
-    Branch <= 2'b0;
-        MemWrite <= 1'b0;
-    MemRead <= 1'b0;
-    MemToReg <= 1'b0;
-    RegDst <= 1'b0;
+    RegWrite <= 1;
+    Branch <= 0;
+        MemWrite <= 0;
+    MemRead <= 0;
+    MemToReg <= 0;
+    RegDst <= 0;
     ALUCntl <= 4'b1010;
-    ALUSrc <= 1'b1;
+    ALUSrc <= 1;
     end
     6'h09:begin //addiu
-        Branch <= 2'b0;
-        RegWrite <= 1'b1;
-        MemWrite <= 1'b0;
-    MemRead <= 1'b0;
-    MemToReg <= 1'b0;
-    RegDst <= 1'b0;
+        Branch <= 0;
+        RegWrite <= 1;
+        MemWrite <= 0;
+    MemRead <= 0;
+    MemToReg <= 0;
+    RegDst <= 0;
     ALUCntl <= 4'b0010;
-    ALUSrc <= 1'b1;
+    ALUSrc <= 1;
     end
     6'h0C:begin //And immediate
-    RegWrite <= 1'b1;
-        Branch <= 2'b0;    
-        MemWrite <= 1'b0;
-    MemRead <= 1'b0;
-    MemToReg <= 1'b0;
-    RegDst <= 1'b0;
+    RegWrite <= 1;
+        Branch <= 0;    
+        MemWrite <= 0;
+    MemRead <= 0;
+    MemToReg <= 0;
+    RegDst <= 0;
     ALUCntl <= 4'b0000;
-    ALUSrc <= 1'b1;
+    ALUSrc <= 1;
     end
     6'h0D:begin //Ori
-    RegWrite <= 1'b1;
-        Branch <= 2'b0;
-        MemWrite <= 1'b0;
-    MemRead <= 1'b0;
-    MemToReg <= 1'b0;
-    RegDst <= 1'b0;
-    ALUSrc <= 1'b1;
-    MemWrite <= 1'b0;
+    RegWrite <= 1;
+        Branch <= 0;
+        MemWrite <= 0;
+    MemRead <= 0;
+    MemToReg <= 0;
+    RegDst <= 0;
+    ALUSrc <= 1;
+    MemWrite <= 0;
     ALUCntl <= 4'b0001;
     end
     6'h23:begin //lw
-        Branch <= 2'b0;
-    RegDst <= 1'b0;
-    RegWrite <= 1'b1;
-    ALUSrc <= 1'b1;
+        Branch <= 0;
+    RegDst <= 0;
+    RegWrite <= 1;
+    ALUSrc <= 1;
     ALUCntl <= 4'b1010;
-    MemWrite <= 1'b0;
-    MemRead <= 1'b1;
-    MemToReg <= 1'b1;
+    MemWrite <= 0;
+    MemRead <= 1;
+    MemToReg <= 1;
     end
     6'h2B:begin //sw
-        Branch <= 2'b0;
-    RegDst <= 1'b0;
-    RegWrite <= 1'b0;
-    ALUSrc <= 1'b1;
+        Branch <= 0;
+    RegDst <= 0;
+    RegWrite <= 0;
+    ALUSrc <= 1;
     ALUCntl <= 4'b1010;
-    MemWrite <= 1'b1;
-    MemRead <= 1'b0;
-    MemToReg <= 1'b0;
+    MemWrite <= 1;
+    MemRead <= 0;
+    MemToReg <= 0;
     end
     6'h04:begin //beq
-    RegDst <= 1'b0;
-    Branch <= 2'b01;
-    RegWrite <= 1'b0;
+    RegDst <= 0;
+    Branch <= 2'b10;
+    RegWrite <= 0;
     ALUCntl <= 4'b1110;
-    ALUSrc <= 1'b0;
-    MemWrite <= 1'b0;
-    MemRead <= 1'b0;
-    MemToReg <= 1'b0;
+    ALUSrc <= 0;
+    MemWrite <= 0;
+    MemRead <= 0;
+    MemToReg <= 0;
     end
     6'h05:begin //bne
-    RegDst <= 1'b0;
-    Branch <= 2'b10;
-    RegWrite <= 1'b0;
+    RegDst <= 0;
+    Branch <= 2'b01;
+    RegWrite <= 0;
     ALUCntl <= 4'b1110;
-    ALUSrc <= 1'b0;
-    MemWrite <= 1'b0;
-    MemRead <= 1'b0;
-    MemToReg <= 1'b0;
+    ALUSrc <= 0;
+    MemWrite <= 0;
+    MemRead <= 0;
+    MemToReg <= 0;
     end
     6'h0A:begin //slti
-    Branch <= 2'b0;
-    RegWrite <= 1'b1;
-        MemWrite <= 1'b0;
-    MemRead <= 1'b0;
-    MemToReg <= 1'b0;
-    RegDst <= 1'b0;
+    Branch <= 0;
+    RegWrite <= 1;
+        MemWrite <= 0;
+    MemRead <= 0;
+    MemToReg <= 0;
+    RegDst <= 0;
     ALUCntl <= 4'b1111;
-    ALUSrc <= 1'b1;
+    ALUSrc <= 1;
     end
     6'h0B:begin //sltiu
-    Branch <= 2'b0;
-    RegWrite <= 1'b1;
-        MemWrite <= 1'b0;
-    MemRead <= 1'b0;
-    MemToReg <= 1'b0;
-    RegDst <= 1'b0;
+    Branch <= 0;
+    RegWrite <= 1;
+        MemWrite <= 0;
+    MemRead <= 0;
+    MemToReg <= 0;
+    RegDst <= 0;
     ALUCntl <= 4'b0100;
-    ALUSrc <= 1'b1;
-    end
-    default:begin
-        Branch <= 2'b0;
-    RegWrite <= 1'b0;
-        MemWrite <= 1'b0;
-    MemRead <= 1'b0;
-    MemToReg <= 1'b0;
-    RegDst <= 1'b0;
-    ALUCntl <= 4'b0100;
-    ALUSrc <= 1'b1;
-    //$display("Error! check control file");
+    ALUSrc <= 1;
     end
     endcase
     end
